c5712c8a0a2d2a87595c286f51476e08ae32a2e6
code-sport <code-sport@t-online.de>
2017-11-25 20:16:12 +0100

    init

---
+++ PiwikAdminShare.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class PiwikAdminShare
+{
+    /**
+     * Attached to activate_{ plugin_basename( __FILES__ ) } by register_activation_hook()
+     * @static
+     */
+    public static function plugin_activation()
+    {
+
+    }
+
+    /**
+     * Removes all connection options
+     * @static
+     */
+    public static function plugin_deactivation()
+    {
+
+    }
+
+}
